7a1ec3fbb5bca210be8d019cd345da50ff8cc347
Clipper4rancione <meluziis.ciro99@gmail.com>
2022-10-10 15:21:46 +0200

    [add] first scripting

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// numero fisso fine password
+
+const anno = 22;
+
+// Chiedere nome dell'utente
+
+const nomeUtente = prompt('Come ti chiami?');
+
+// Chiedere cognome all'utente
+
+const cognomeUtente = prompt("Qual'è il tuo cognome?");
+
+// Chiedere colore preferito
